b59bf5af40b22a5e6ad01b28debf5f8b86d69448
Edgar Medina <is715468@iteso.mx>
2020-09-30 21:48:19 -0500

    Se agregó función para enviar datos a backend

--- public/js/login.js
+++ public/js/login.js
@@ -41,3 +41,39 @@ formLogin.addEventListener("change", function (E) {
     }
 });
 
+
+let sendData = function() {
+    console.log('Si entré wey');
+
+    let data = {
+        email : email.value,
+        password : password.value
+    };
+
+    // 1. Crear XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+    // 2. Configurar: POST actualizar archivo
+    xhr.open('POST','https://localhost:8080/api/login');
+    // 3. indicar tipo de datos JSON
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    
+    // 4. Enviar solicitud al servidor
+    xhr.send(JSON.stringify(data));
+
+    console.log(JSON.stringify(data));
+    // 5. Una vez recibida la respuesta del servidor
+    xhr.onload = function () {
+        if (xhr.status != 200) { // analizar el estatus de la respuesta HTTP
+            // Ocurrió un error
+            //alert(xhr.status + ': ' + xhr.statusText); // e.g. 404: Not Found
+            alert(xhr.status + ': ' + xhr.statusText);
+        
+        } else {
+            //console.log(xhr.responseText); // Significa que fue exitoso
+            alert(xhr.responseText);
+            window.location.href = "../index.html";
+        }
+    };
+
+    return false;
+}
