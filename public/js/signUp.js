df6159b6637ebc00906d4b472dacc7244c612d6f
Edgar Medina <is715468@iteso.mx>
2020-09-30 21:49:05 -0500

    Se agregó función para enviar datos a backend

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,3 +1,4 @@
+let email = document.getElementById('Email');
 let password = document.getElementById('password');
 let confirmPassword = document.getElementById('confirm_password');
 let spanList = document.querySelectorAll('form span');
@@ -53,3 +54,40 @@ formSign.addEventListener("change", function (E) {
         submit.disabled = false;
     }
 });
+
+
+let sendData = function() {
+    console.log('Si entré wey');
+
+    let data = {
+        email : email.value,
+        password : password.value
+    };
+
+    // 1. Crear XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+    // 2. Configurar: POST actualizar archivo
+    xhr.open('POST','https://localhost:8080/api/signup');
+    // 3. indicar tipo de datos JSON
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    
+    // 4. Enviar solicitud al servidor
+    xhr.send(JSON.stringify(data));
+
+    console.log(JSON.stringify(data));
+    // 5. Una vez recibida la respuesta del servidor
+    xhr.onload = function () {
+        if (xhr.status != 201) { // analizar el estatus de la respuesta HTTP
+            // Ocurrió un error
+            //alert(xhr.status + ': ' + xhr.statusText); // e.g. 404: Not Found
+            alert(xhr.status + ': ' + xhr.statusText);
+        
+        } else {
+            //console.log(xhr.responseText); // Significa que fue exitoso
+            alert(xhr.responseText);
+            window.location.href = "../index.html";
+        }
+    };
+
+    return false;
+}
