f4e9eda1dc3dbb42a6eef9a7d4fc0a55a395bfbc
Edgar Medina <is715468@iteso.mx>
2020-10-02 15:25:15 -0500

    js para cargar la barra de navegación

---
+++ public/js/main.js
@@ -0,0 +1,41 @@
+let navbar = document.getElementById('navBar');
+
+let cerrarSesion = function () {
+    sessionStorage.login = false;
+    window.location.href = "../index.html";
+
+}
+
+if (sessionStorage.login == 'true') {
+    navBar.innerHTML = `<a href="./index.html">
+    TLS project
+    </a>
+    <a href="./views/home.html">
+        Home
+    </a>
+    <a href="./views/upload.html">
+        Upload
+    </a>
+    <a href="">
+        Contact
+    </a>
+    <a class="login" id="idLogin" onclick="cerrarSesion()">
+        ${sessionStorage.email}
+    </a>`
+} else {
+    navBar.innerHTML = `<a href="./index.html">
+    TLS project
+    </a>
+    <a href="./views/home.html">
+        Home
+    </a>
+    <a href="./views/upload.html">
+        Upload
+    </a>
+    <a href="">
+        Contact
+    </a>
+    <a class="login" href="./views/login.html" id="idLogin">
+        Logged in as Username
+    </a>`
+}
