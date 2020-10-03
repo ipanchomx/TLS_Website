229751d71732ad770e9fbdc55a01d2e846840e04
Edgar Medina <is715468@iteso.mx>
2020-10-02 19:47:13 -0500

    Se agregaron endpoints para firmar docs y verificar

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const https = require("https");
 const fs = require("fs");
 const path = require("path");
 const fileUpload = require('express-fileupload');
+const crypto = require('crypto');
 
 let mongoose = require('mongoose');
 let config = require('./config/config');
@@ -13,6 +14,12 @@ let PORT = 8080;
 
 
 const app = express();
+
+// Private key
+const private_key = fs.readFileSync(path.join(__dirname, 'keys/privateKey.pem'), 'utf-8');
+
+// Public key
+const public_key  = fs.readFileSync(path.join(__dirname, 'keys/publicKey.pem'), 'utf-8');
 
 // default options
 app.use(fileUpload());
@@ -52,9 +59,7 @@ app.post('/upload', function (req, res) {
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     let sampleFile = req.files.file;
 
-    //console.log('sampleFile: ',sampleFile);
-    //console.log('sampleFile2: ',sampleFile);
-    const routeFilename = path.join(__dirname, 'uploads', sampleFile.name);
+    const routeFilename = path.join(__dirname, 'uploads/', sampleFile.name);
 
     // Use the mv() method to place the file somewhere on your server
     sampleFile.mv(routeFilename, function (err) {
@@ -69,9 +74,62 @@ app.post('/upload', function (req, res) {
                 filename : sampleFile.name, 
                 filePath : routeFilename
             });
+
+            // File/Document to be signed
+            const doc = fs.readFileSync(path.join(__dirname, 'uploads/' + sampleFile.name));
+
+            // Signing
+
+            const signer = crypto.createSign('RSA-SHA256');
+            signer.write(doc);
+            signer.end();
+
+            // Returns the signature in output_format which can be 'binary', 'hex' or 'base64'
+            const signature = signer.sign(private_key, 'base64')
+
+            console.log('Digital Signature: ', signature);
+
+            // Write signature to the file `signature.txt`
+            fs.writeFileSync(path.join(__dirname, 'signatures/', sampleFile.name), signature);
         }
     });
+
 });
+
+app.post('/verify', function(req, res) {
+    //console.log(req.body);
+
+    const signature = fs.readFileSync(path.join(__dirname, 'signatures/'+ req.body.name), 'utf-8');
+
+    //console.log('signature: ', signature);
+    // File to be signed
+    const doc = fs.readFileSync(path.join(__dirname, 'uploads/' + req.body.name));
+
+    //console.log('doc: ', doc);
+
+    // Signing
+    const verifier = crypto.createVerify('RSA-SHA256');
+    verifier.write(doc);
+    verifier.end();
+
+    // Verify file signature ( support formats 'binary', 'hex' or 'base64')
+    const result = verifier.verify(public_key, signature, 'base64');
+
+    console.log('Digital Signature Verification : ' + result);    
+
+    if(result) {
+        res.status(200).send({
+            message : 'Verification completed!',
+            status: 'OK!'
+        });
+    } else {
+        res.status(202).send({
+            message : 'Verification completed!',
+            status: 'Cuidado! La firma no coincide con el archivo.'
+        }); 
+    }
+});
+
 
 app.get('/getFiles', function(req, res) {
     let url = path.join(__dirname, 'uploads');
@@ -83,6 +141,7 @@ app.get('/getFiles', function(req, res) {
         filepath : url
     });
 });
+
 
 mongoose.connect(config.DB_URL, {
         useCreateIndex: true,
