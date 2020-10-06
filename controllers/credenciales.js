cba562cff64aaee66ac4e67d45fdd2eaf73e7e64
Edgar Medina <is715468@iteso.mx>
2020-10-06 16:16:54 -0500

    Se agregó funcionalidad para validar un login con autenticación

--- controllers/credenciales.js
+++ controllers/credenciales.js
@@ -1,6 +1,8 @@
 'use strict'
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const speakeasy = require("speakeasy");
+const QRCode = require('qrcode');
 
 let Credenciales = require('../models/credenciales');
 
@@ -35,9 +37,12 @@ let createCredencial = function (req, res) {
         .then(user => {
             if(!user) {
                 const hash = bcrypt.hashSync(password, saltRounds);
+                let actualDate = new Date();
                 Credenciales.create({
                     email: email,
-                    password: hash
+                    password: hash,
+                    lastConnection : actualDate,
+                    countLogin : 0
                 }, (err) => {
                     if (err) {
                         console.log(err);
@@ -60,18 +65,54 @@ let getCredencial = function(req, res) {
         email,
         password
     } = req.body;
-/*
-    console.log("Usuario a loggearse");
-    console.log(req.body);*/
+
     Credenciales.findOne({email: email})
         .then(user => {
             if(user) {
-                /*
-                console.log("Usuario en base de datos");                
-                console.log(user);                
-                console.log(bcrypt.compareSync(password, user.password)); */
-                // true
-                if(bcrypt.compareSync(password, user.password)) {
+
+                let secret = speakeasy.generateSecret({
+                    name :"TLS_Website"
+                });
+                // Returns an object with secret.ascii, secret.hex, and secret.base32.
+                // Also returns secret.otpauth_url, which we'll use later.
+                console.log('El token: ',secret.ascii);
+                Credenciales.findOneAndUpdate({email : user.email}, {token : secret.ascii}, function(req, res) {});
+
+                // Get the data URL of the authenticator URL
+                QRCode.toDataURL(secret.otpauth_url, function(err, data_url) {
+                    res.status(200).send({url : data_url});
+                });         
+            }
+        }).catch(err => {
+            console.log(err);
+            res.status(500).send({message : 'Server error.', error : `${err}`});
+        })
+}
+
+let verifyToken = function(req, res) {
+    let {
+        email,
+        password, 
+        codigo
+    } = req.body;
+
+    Credenciales.findOne({email: email})
+        .then(user => {
+            if(user) {                
+
+                console.log(user);
+                let tokenPage = user.token;
+                // Use verify() to check the token against the secret
+                let verified = speakeasy.totp.verify({ secret: tokenPage,
+                    encoding: 'ascii',
+                    token: codigo, 
+                });
+                    
+                if(bcrypt.compareSync(password, user.password) && verified) {
+                    console.log('Entró al if we');
+                    Credenciales.findOneAndUpdate({email : user.email}, {lastConnection : new Date(), countLogin : user.countLogin + 1}, function(req, res) {
+                    });
+
                     res.status(200).send({message :'Usuario autorizado', 
                     email : user.email
                 });
@@ -80,12 +121,12 @@ let getCredencial = function(req, res) {
         }).catch(err => {
             console.log(err);
             res.status(500).send({message : 'Server error.', error : `${err}`});
-        })
+        })    
 }
-
 
 module.exports = {
     getCredenciales,
     createCredencial,
-    getCredencial
+    getCredencial,
+    verifyToken
 }
