5f02f4b257c4767e7f62102fde1e1a55fc483911
Edgar Medina <is715468@iteso.mx>
2020-10-07 00:08:28 -0500

    Nuevo endpoint para editar información de la cuenta con el inicio de sesión activo

--- routes/credenciales.js
+++ routes/credenciales.js
@@ -11,6 +11,7 @@ api.get('/credenciales', CredencialesController.getCredenciales);
 api.post('/signup', CredencialesController.createCredencial);
 api.post('/login', CredencialesController.getCredencial);
 api.post('/verifyToken', CredencialesController.verifyToken);
+api.post('/editInfo', CredencialesController.editInfo);
 
 
 module.exports = api;
