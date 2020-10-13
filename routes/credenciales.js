30a01e742228ed0177e271ef28b9b37271f3f8f7
Edgar Medina <is715468@iteso.mx>
2020-10-12 21:23:30 -0500

    Cambios menores

--- routes/credenciales.js
+++ routes/credenciales.js
@@ -7,11 +7,9 @@ const CredencialesController = require('../controllers/credenciales');
 let api = express.Router();
 
 api.get('/credenciales', CredencialesController.getCredenciales);
-
 api.post('/signup', CredencialesController.createCredencial);
 api.post('/login', CredencialesController.getCredencial);
 api.post('/verifyToken', CredencialesController.verifyToken);
 api.post('/editInfo', CredencialesController.editInfo);
-
 
 module.exports = api;
